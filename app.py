2659eab924014dbf9aa98ac65fa5f6852e7e93a5
guigsguigs <36706220+guigsguigs@users.noreply.github.com>
2024-03-11 17:48:27 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, flash
+from werkzeug.utils import secure_filename
+import pandas as pd
+import os
+
+app = Flask(__name__)
+app.secret_key = 'une_cle_secrete'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files.get('file')
+        if file:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            valid_emails = extract_emails(filepath)
+            if valid_emails:
+                emails_str = ";".join(valid_emails)
+                flash(f"{len(valid_emails)} adresses email extraites avec succès.")
+                return render_template('emails.html', emails=emails_str)
+            else:
+                flash("Aucun e-mail valide trouvé.")
+    return render_template('upload.html')
+
+def extract_emails(filepath):
+    try:
+        df = pd.read_csv(filepath, sep=';', skiprows=1)
+        valid_emails = df['email'].dropna().apply(lambda x: x if "@" in x else None).dropna().tolist()
+        return valid_emails
+    except Exception as e:
+        flash(f"Une erreur est survenue lors de l'extraction des e-mails : {e}")
+        return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
