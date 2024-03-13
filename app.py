0bd17a6bccd7b34b2d4bcdfe98cef87415d88058
guigsguigs <36706220+guigsguigs@users.noreply.github.com>
2024-03-13 13:38:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,22 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files.get('file')
-        if file:
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            valid_emails = extract_emails(filepath)
-            if valid_emails:
-                emails_str = ";".join(valid_emails)
-                flash(f"{len(valid_emails)} adresses email extraites avec succès.")
-                return render_template('emails.html', emails=emails_str)
-            else:
-                flash("Aucun e-mail valide trouvé.")
+        files = request.files.getlist('file')  # Obtenir une liste de fichiers
+        all_valid_emails = []  # Liste pour cumuler les emails valides de tous les fichiers
+        for file in files:
+            if file:
+                filename = secure_filename(file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
+                valid_emails = extract_emails(filepath)
+                if valid_emails:
+                    all_valid_emails.extend(valid_emails)  # Ajoute les emails valides de ce fichier à la liste cumulée
+        if all_valid_emails:
+            emails_str = ";".join(all_valid_emails)
+            flash(f"{len(all_valid_emails)} adresses email extraites avec succès.")
+            return render_template('emails.html', emails=emails_str)
+        else:
+            flash("Aucun e-mail valide trouvé.")
     return render_template('upload.html')
 
 def extract_emails(filepath):
